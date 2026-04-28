7e257b93a728d14a34d9fc554cdb0ab1d4082372
Cideval <valleriandicque10@gmail.com>
2026-04-28 19:40:00 +0200

    feat: first commit

---
+++ Gastronomie/Gastronomie/GastronomieApp.swift
@@ -0,0 +1,17 @@
+//
+//  GastronomieApp.swift
+//  Gastronomie
+//
+//  Created by apprenant76 on 28/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct GastronomieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
