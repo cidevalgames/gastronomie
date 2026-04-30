74c30ab4c25bbcdc83ea1d2a7cbbc8a9545b9a2c
Geo Duc <unknowgeo17@hotmail.fr>
2026-04-30 10:13:56 +0200

    feat: create Badge model

---
+++ Gastronomie/Models/Badge.swift
@@ -0,0 +1,13 @@
+//
+//  Badge.swift
+//  Gastronomie
+//
+//  Created by apprenant96 on 30/04/2026.
+//
+
+import Foundation
+
+struct Badge: Identifiable {
+    let id: UUID = UUID()
+    let image : String
+}
