9e7e9a8117b3cfca12784313fc3727b318ad15ca
Leyna Nkounkou <leynankounkou@gmail.com>
2026-04-30 10:21:29 +0200

    feat: create Gift model

---
+++ Gastronomie/Models/Gift.swift
@@ -0,0 +1,23 @@
+//
+//  Gift.swift
+//  Gastronomie
+//
+//  Created by apprenant99 on 30/04/2026.
+//
+
+import Foundation
+struct Gift: Identifiable {
+    let id: UUID = UUID()
+    let image: String
+}
+
+//let gifts: [Gift] = [
+//    Gift(image: "gift1"),
+//    Gift(image: "gift2"),
+//    Gift(image: "gift3"),
+//    Gift(image: "gift4"),
+//    Gift(image: "gift5"),
+//    Gift(image: "gift6"),
+//    Gift(image: "gift7"),
+//    Gift(image: "gift8"),
+//    Gift(image: "gift9")
