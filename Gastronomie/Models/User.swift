3d96826f7cb297859eda9070d0692f282039ded7
Cideval <valleriandicque10@gmail.com>
2026-05-04 13:51:22 +0200

    fix(user-model): replace registered recipes by an array of recipes

--- Gastronomie/Models/User.swift
+++ Gastronomie/Models/User.swift
@@ -23,35 +23,27 @@ struct User: Identifiable {
     let xp: Int
     let points: Int
     let rank: Int
-    var registeredRecipes: [UUID]
-    // Pas encore de badge
+    var registeredRecipes: [Recipe]
     //let badges: [Badge]
 }
 
 extension User {
-    static var preview: User = User(
-        firstName: "Marc",
-        lastName: "Leroy",
-        avatarImage: "avatar1",
-        genre: .man,
-        xp: 5900,
-        points: 750,
-        rank: 56,
-        registeredRecipes: []
-    )
+    static var preview: User {
+        return User()
+    }
     
     init(recipeService: RecipeProvider = RecipeService()) {
-        let recipesIDs: [UUID] = Array(recipeService.fetchAll().prefix(6).map{$0.id})
+        let recipes: [Recipe] = Array(recipeService.fetchAll().prefix(6))
         
         self.init(
-            firstName: User.preview.firstName,
-            lastName: User.preview.lastName,
-            avatarImage: User.preview.avatarImage,
-            genre: User.preview.genre,
-            xp: User.preview.xp,
-            points: User.preview.points,
-            rank: User.preview.rank,
-            registeredRecipes: recipesIDs
+            firstName: "Marc",
+            lastName: "Leroy",
+            avatarImage: "avatar1",
+            genre: .man,
+            xp: 5900,
+            points: 750,
+            rank: 56,
+            registeredRecipes: recipes
         )
     }
 }
