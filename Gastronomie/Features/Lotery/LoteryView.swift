4e940041145703e95709739bddc525eb6d303838
Fatoumata Binta Diallo <bintadiallosadjo027@gmail.com>
2026-05-04 14:34:02 +0200

    feat(view): implement lotery view

---
+++ Gastronomie/Features/Lotery/LoteryView.swift
@@ -0,0 +1,72 @@
+//
+//  LoteryView.swift
+//  Gastronomie
+//
+//  Created by apprenant109 on 30/04/2026.
+//
+
+import SwiftUI
+
+struct LoteryView: View {
+    
+    var body: some View {
+        NavigationStack {
+            
+            VStack(alignment: .center, spacing: 20) {
+                Text("Loterie")
+                    .font(.pageTitle)
+                    .bold()
+                
+                Text("Ce que les participants ont gagné...")
+                    .font(.pageTitle)
+                    .foregroundStyle(.richMahogany)
+                    .padding()
+                    .multilineTextAlignment(.center)
+                
+                InfiniteCarousel()
+                    
+                Text("Et bien d'autres...")
+                    .font(.pageTitle)
+                    .padding()
+                    .foregroundStyle(.richMahogany)
+                HStack(spacing: 4) {
+                    Text("Solde: 750")
+                        .font(.loteryMoney)
+                        .foregroundStyle(.darkWine)
+                    Image(systemName: "bitcoinsign.circle")
+                        .font(.loteryMoney)
+                        .foregroundStyle(.darkWine)
+                }
+                
+                Button {
+                    print("do something")
+                    
+                    
+                } label: {
+                 
+                    HStack {
+                        Spacer()
+                        Text("Participer pour 500")
+                            .foregroundStyle(.white)
+                            .font(.button)
+                        Image(systemName: "bitcoinsign.circle")
+                            .font(.loteryMoney)
+                            .foregroundStyle(.white)
+                           
+                        Spacer()
+                    }
+                    .padding()
+                    .background(.darkWine)
+                    .cornerRadius(40)
+                    .padding()
+                }
+                
+                Spacer()
+                
+            }
+        }
+    }
+}
+#Preview {
+    LoteryView()
+}
