1f724eef0727bb36adf7b9ac3a98a781230efa7f
Cideval <valleriandicque10@gmail.com>
2026-05-04 16:57:52 +0200

    feat(view-model): implement challenges in challenge view model and integrate it in challenge view

--- Gastronomie/Features/Challenge/ChallengeView.swift
+++ Gastronomie/Features/Challenge/ChallengeView.swift
@@ -8,28 +8,32 @@
 import SwiftUI
 
 struct ChallengeView: View {
+    @State var viewModel: ChallengeViewModel = ChallengeViewModel()
+    
     let columns = [GridItem(),GridItem()]
     
     var body: some View {
-            VStack {
-                ScrollView(.vertical) {
+        VStack {
+            ScrollView(.vertical) {
                 Text("Défis actuels")
                     .font(.pageTitle)
                     .foregroundStyle(.richMahogany)
                     .padding()
-                ChallengeRow(score: 10, text: "Cuisiner une recette avec une difficulté de 3 étoiles")
-                ChallengeRow(score: 20, text: "Mettre une recette en favoris")
-                ChallengeRow(score: 50, text: "Participer à un tirage au sort" )
-
+                
+                VStack {
+                    ForEach(viewModel.threeChallengesList) { challenge in
+                        ChallengeRow(points: challenge.points, title: challenge.title)
+                    }
+                }
+                
                 Text("Challenges réalisés")
                     .font(.pageTitle)
                     .foregroundStyle(.richMahogany)
                     .padding()
                 
-                    LazyVGrid(columns: columns) {
-                        ForEach(BadgeService().fetchAll()) { badge in
-                            BadgeCard(badge: badge)
-                        
+                LazyVGrid(columns: columns) {
+                    ForEach(BadgeService().fetchAll()) { badge in
+                        BadgeCard(badge: badge)
                     }
                 }
             }
