6aa3b3e4105326ec41cf2e0a3de10ff47a22f087
Cideval <valleriandicque10@gmail.com>
2026-05-04 16:41:36 +0200

    feat(service): add badges to user service

--- Gastronomie/Services/UserService.swift
+++ Gastronomie/Services/UserService.swift
@@ -17,20 +17,21 @@ protocol SessionProvider {
 
 struct UserService: UserProvider, SessionProvider {
     private var allUsers: [User] = [
-        User(firstName: "Marc", lastName: "Leroy", avatarImage: "avatar1", genre: .man, xp: 5900, points: 750, rank: 56, registeredRecipes: []),
-        User(firstName: "Lucas", lastName: "Bernard", avatarImage: "avatar2", genre: .man, xp: 13000, points: 2500, rank: 1, registeredRecipes: []),
-        User(firstName: "Emma", lastName: "Richard", avatarImage: "avatar3", genre: .woman, xp: 12300, points: 1800, rank: 2, registeredRecipes: []),
-        User(firstName: "Nathan", lastName: "Durand", avatarImage: "avatar4", genre: .man, xp: 11775, points: 1200, rank: 3, registeredRecipes: []),
-        User(firstName: "Chloé", lastName: "Morel", avatarImage: "avatar5", genre: .woman, xp: 6100, points: 800, rank: 55, registeredRecipes: []),
-        User(firstName: "Léa", lastName: "Petit", avatarImage: "avatar6", genre: .woman, xp: 5700, points: 300, rank: 57, registeredRecipes: [])
+        User(firstName: "Marc", lastName: "Leroy", avatarImage: "avatar1", genre: .man, xp: 5900, points: 750, rank: 56, registeredRecipes: [], badges: []),
+        User(firstName: "Lucas", lastName: "Bernard", avatarImage: "avatar2", genre: .man, xp: 13000, points: 2500, rank: 1, registeredRecipes: [], badges: []),
+        User(firstName: "Emma", lastName: "Richard", avatarImage: "avatar3", genre: .woman, xp: 12300, points: 1800, rank: 2, registeredRecipes: [], badges: []),
+        User(firstName: "Nathan", lastName: "Durand", avatarImage: "avatar4", genre: .man, xp: 11775, points: 1200, rank: 3, registeredRecipes: [], badges: []),
+        User(firstName: "Chloé", lastName: "Morel", avatarImage: "avatar5", genre: .woman, xp: 6100, points: 800, rank: 55, registeredRecipes: [], badges: []),
+        User(firstName: "Léa", lastName: "Petit", avatarImage: "avatar6", genre: .woman, xp: 5700, points: 300, rank: 57, registeredRecipes: [], badges: [])
     ]
     
     var currentUser: User
     
-    init(recipeService: RecipeProvider = RecipeService()) {
+    init(recipeService: RecipeProvider = RecipeService(), badgeService: BadgeProvider = BadgeService()) {
         self.currentUser = allUsers[0]
         // Select 6 first recipes just for mockup
         self.currentUser.registeredRecipes = Array(recipeService.fetchAll().prefix(6))
+        self.currentUser.badges = Array(badgeService.fetchAll())
     }
     
     func fetchAll() -> [User] {
