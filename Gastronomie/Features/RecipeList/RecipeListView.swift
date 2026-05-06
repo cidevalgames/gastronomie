5182a4c3303d55cdb3f65532616ea2973bceb0ae
Cideval <valleriandicque10@gmail.com>
2026-05-06 09:54:52 +0200

    feat(view): add navigation links to recipe list view

--- Gastronomie/Features/RecipeList/RecipeListView.swift
+++ Gastronomie/Features/RecipeList/RecipeListView.swift
@@ -10,51 +10,56 @@ struct RecipeListView: View {
     @State var viewModel: RecipeListViewModel = RecipeListViewModel()
     
     var body: some View {
-        ScrollView {
-            // Présentation de l'utilisateur
-            
-            VStack {
-                Text("Bonjour,")
-                    .font(.pageTitle)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .foregroundStyle(.richMahogany)
+        NavigationStack {
+            ScrollView {
+                // Présentation de l'utilisateur
                 
-                Text(viewModel.currentUserName)
-                    .font(.pageTitle)
-                    .fontWeight(.bold)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .foregroundStyle(.richMahogany)
-                
-                Spacer()
-                
-                // Recette du jour
-                
-                Text("La recette du jour")
-                    .font(.pageTitle)
-                    .foregroundStyle(.richMahogany)
-                
-                HStack {
+                VStack(spacing: 16) {
+                    VStack {
+                        Text("Bonjour,")
+                            .font(.pageTitle)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .foregroundStyle(.richMahogany)
+                        
+                        Text(viewModel.currentUserName)
+                            .font(.pageTitle)
+                            .fontWeight(.bold)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .foregroundStyle(.richMahogany)
+                    }
                     
-                    // rajouter icone étoiles
+                    Spacer()
                     
-                    RecipeCard(title: viewModel.dailyRecipe.title, image: viewModel.dailyRecipe.image, description: viewModel.dailyRecipe.description)
+                    // Recette du jour
+                    
+                    Text("La recette du jour")
+                        .font(.pageTitle)
+                        .foregroundStyle(.richMahogany)
+                    
+                    NavigationLink {
+                        RecipeDetailView(recipe: viewModel.dailyRecipe)
+                    } label: {
+                        RecipeCard(recipe: viewModel.dailyRecipe)
+                    }
+                    
+                    Divider()
+                    
+                    // Section Recommandations
+                    
+                    Text("Recommandations")
+                        .font(.pageTitle)
+                        .foregroundStyle(.richMahogany)
+                    
+                    ForEach(viewModel.recipes) { recipe in
+                        NavigationLink {
+                            RecipeDetailView(recipe: recipe)
+                        } label: {
+                            RecipeCard(recipe: recipe)
+                        }
+                    }
                 }
-                .padding(6)
-                .background(.brightSnow)
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-                .shadow(color: .black.opacity(0.25), radius: 8)
-                
-                // Section Recommandations
-                
-                Text("Recommandations")
-                    .font(.pageTitle)
-                    .foregroundStyle(.richMahogany)
-                
-                ForEach(viewModel.recipes) { recipe in
-                    RecipeCard(title: recipe.title, image: recipe.image, description: recipe.description)
-                }
+                .padding()
             }
-            .padding()
         }
     }
 }
