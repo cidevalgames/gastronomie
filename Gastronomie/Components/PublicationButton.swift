5f71bcb2805f1d807edc00fab0936a6de330c3d0
Fatoumata Binta Diallo <bintadiallosadjo027@gmail.com>
2026-05-07 15:03:34 +0200

    feat: create validation modal and integrate it in publication view

---
+++ Gastronomie/Components/PublicationButton.swift
@@ -0,0 +1,18 @@
+//
+//  PublicationButton.swift
+//  Gastronomie
+//
+//  Created by apprenant109 on 07/05/2026.
+//
+
+import SwiftUI
+
+struct PublicationButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PublicationButton()
+}
