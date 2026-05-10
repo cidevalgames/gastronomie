1a6d151f274064650c936788567dde8bcdd5c762
Cideval <valleriandicque10@gmail.com>
2026-05-10 15:09:22 +0200

    feat(component): create interactable difficulty indicator component and add it in publication view

---
+++ Gastronomie/Features/Publication/PublicationViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  PublicationViewModel.swift
+//  Gastronomie
+//
+//  Created by apprenant76 on 10/05/2026.
+//
+
+import Foundation
+import Observation
+
+@Observable
+class PublicationViewModel {
+    var difficulty: Int = 0
+}
