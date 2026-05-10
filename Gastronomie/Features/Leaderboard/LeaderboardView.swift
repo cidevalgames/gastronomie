3b42bd8c8f849c3e48ba7ae7d4922f800ef86152
Cideval <valleriandicque10@gmail.com>
2026-05-10 17:12:31 +0200

    fix: add scroll view on leaderboard view to avoid image issues

--- Gastronomie/Features/Leaderboard/LeaderboardView.swift
+++ Gastronomie/Features/Leaderboard/LeaderboardView.swift
@@ -13,24 +13,26 @@ struct LeaderboardView: View {
     let isCurrentUser = false
     
     var body: some View {
-        VStack(spacing: 32) {
-            Text("Classement")
-                .font(.pageTitle)
-                .bold()
-                .foregroundStyle(.richMahogany)
-            
-            PodiumView(user1: viewModel.threeFirstUsers[0], user2: viewModel.threeFirstUsers[1], user3: viewModel.threeFirstUsers[2])
-            
-            VStack(spacing: 8) {
-                ForEach(viewModel.usersAroundCurrentUser as! [User]) { user in
-                    LeaderboardCard(user: user, isCurrentUser: viewModel.isCurrentUser(user: user))
+        ScrollView(.vertical) {
+            VStack(spacing: 32) {
+                Text("Classement")
+                    .font(.pageTitle)
+                    .bold()
+                    .foregroundStyle(.richMahogany)
+                
+                PodiumView(user1: viewModel.threeFirstUsers[0], user2: viewModel.threeFirstUsers[1], user3: viewModel.threeFirstUsers[2])
+                
+                VStack(spacing: 8) {
+                    ForEach(viewModel.usersAroundCurrentUser as! [User]) { user in
+                        LeaderboardCard(user: user, isCurrentUser: viewModel.isCurrentUser(user: user))
+                    }
                 }
+                .frame(height: 324)
+                
+                Spacer()
             }
-            .frame(height: 324)
-            
-            Spacer()
+            .padding()
         }
-        .padding()
     }
 }
 
